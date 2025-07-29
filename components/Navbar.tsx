abe1ca56e33f8472f5f3ad8aa700bf682b83391b
Kub1991 <borisyakszy@gmail.com>
2025-07-29 21:59:03 +0200

    Slightly reduce logo size

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -44,13 +44,13 @@ export default function Navbar() {
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <div className="flex items-center gap-3">
-            <div className="relative w-32 h-32 sm:w-40 sm:h-40">
+            <div className="relative w-28 h-28 sm:w-36 sm:h-36">
               <Image
                 src="/Logo_Pracownia_Protetyki_K.Szymanska.v2.png"
                 alt="Logo Pracownia Protetyki"
                 fill
                 className="object-contain"
-                sizes="160px"
+                sizes="144px"
                 priority
               />
             </div>
@@ -94,13 +94,13 @@ export default function Navbar() {
             <SheetContent side="right" className="w-72 sm:w-80">
               <div className="flex flex-col gap-4 sm:gap-6 mt-6 sm:mt-8">
                 <div className="flex items-center gap-3 mb-6">
-                  <div className="relative w-32 h-32 sm:w-40 sm:h-40">
+                  <div className="relative w-28 h-28 sm:w-36 sm:h-36">
                     <Image
                       src="/Logo_Pracownia_Protetyki_K.Szymanska.v2.png"
                       alt="Logo Pracownia Protetyki"
                       fill
                       className="object-contain"
-                      sizes="160px"
+                      sizes="144px"
                     />
                   </div>
                 </div>
