0b0c6c767a5f57aca1c2d86bbfbd2358bc133b33
Kub1991 <borisyakszy@gmail.com>
2025-07-31 13:50:44 +0200

    Update tooth mounting description

--- components/ProblemSection.tsx
+++ components/ProblemSection.tsx
@@ -20,7 +20,7 @@ const problems = [
   {
     icon: <WrenchScrewdriverIcon className="h-6 w-6" />,
     title: "Wypadł ząb z protezy? Wstawię go od ręki.",
-    description: "Szybkie i trwałe mocowanie wypadłych zębów z protezy."
+    description: "Szybkie i trwałe mocowanie zębów w protezie."
   },
   {
     icon: <AdjustmentsHorizontalIcon className="h-6 w-6" />,
